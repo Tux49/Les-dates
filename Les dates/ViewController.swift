d504f6e398ede48e63be59b3dd0c411060f0c62d
Thierry Huu <huuth@macbook-pro-de-thierry.local>
2018-10-18 23:00:39 +0200

    Projet complet

--- Les dates/ViewController.swift
+++ Les dates/ViewController.swift
@@ -11,9 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
+    
+    // Obtenir une String
     @IBOutlet weak var dateStringLbl: UILabel!
+    
+    // TimeInterval
     @IBOutlet weak var timeIntervalLbl: UILabel!
+    
+    // Si Aujourd'hui, demain, hier ou week-end
     @IBOutlet weak var calendarLbl: UILabel!
+    
+    // Il y a + mois, jours, heures
     @IBOutlet weak var ilyaLbl: UILabel!
     
     override func viewDidLoad() {
@@ -21,5 +29,63 @@ class ViewController: UIViewController {
     }
 
     @IBAction func dateChoisie(_ sender: UIDatePicker) {
+        let date = sender.date
+        print(date)
+        dateStringLbl.text = date.toString()
+        timeIntervalLbl.text = date.interval()
+        calendarLbl.text = date.calendarString()
+        ilyaLbl.text = date.components()
+    }
+}
+
+extension Date {
+    
+    func toString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "fr_FR")
+        return dateFormatter.string(from: self)
+    }
+    
+    func interval() -> String {
+        return String(format: "%.2f", self.timeIntervalSinceNow)
+    }
+    
+    func calendarString() -> String {
+        var str = ""
+        
+        let calendar = Calendar.current
+        
+        if calendar.isDateInToday(self) {
+            str += "Aujourd'hui"
+        }
+        
+        if calendar.isDateInTomorrow(self) {
+            str += "Demain"
+        }
+        
+        if calendar.isDateInYesterday(self) {
+            str += "Hier"
+        }
+        
+        if calendar.isDateInWeekend(self) {
+            str += "Nous sommes le week-end"
+        }
+        
+        return str
+    }
+    
+    func components() -> String {
+        
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.month, .day, .hour, .minute], from: self, to: Date())
+        
+        let mois = components.month ?? 0
+        let jour = components.day ?? 0
+        let heure = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
+        return "Temps écoulé -> mois: \(mois), jour(s): \(jour), heure(s): \(heure), minute(s): \(minute)"
     }
 }
